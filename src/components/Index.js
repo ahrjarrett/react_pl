d25ee3ccef403b2249c873c4eaf3b1ef5ced279a
Andrew Jarrett <ahrjarrett@gmail.com>
2017-10-24 13:51:58 -0600

    wire up ArticleList & Article components

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,26 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-class App extends Component {
-  state = {
-    answer: 49
-  }
-
-  asyncFunc = () => {
-    return Promise.resolve(52)
-  }
-
-  async componentDidMount() {
-    this.setState({ answer: await this.asyncFunc() })
-  }
-
-  render() {
-    return (
-      <div>
-      <h2> class component stuffs {this.state.answer} </h2>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<App/>, document.getElementById('root'))
